605c13ad79ff4aeafcac4e2cd084cf175da58228
namespace-Pt <zpt@ruc.edu.cn>
2021-08-30 11:16:23 +0800

    1

--- Code/scripts/daemon.py
+++ Code/scripts/daemon.py
@@ -1,11 +1,15 @@
 import torch
 import time
 import logging
+logging.basicConfig(level=logging.INFO,
+                    format="[%(asctime)s] %(levelname)s (%(name)s) %(message)s")
+logger = logging.getLogger(__file__)
 
-a = torch.rand((1,3),device=1)
-logging.info("I'm running to stop the platform killing this job!")
-while 1:
-    if a.sum() < 100:
-        a += 1
-    else:
-        a -= 1
+a = torch.zeros((1),device=1)
+logger.info("I'm running to stop the platform killing this job!")
+with torch.no_grad():
+    while 1:
+        if a[0] < 1:
+            a += 1
+        else:
+            a -= 1
