1baace3aeec414ab347a3cbb45ad494ead9c61e4
namespace-Pt <zpt@ruc.edu.cn>
2021-10-08 23:47:01 +0800

    1

--- Code/scripts/daemon.py
+++ Code/scripts/daemon.py
@@ -5,11 +5,7 @@ logging.basicConfig(level=logging.INFO,
                     format="[%(asctime)s] %(levelname)s (%(name)s) %(message)s")
 logger = logging.getLogger(__file__)
 
-a = torch.zeros((1),device=1)
 logger.info("I'm running to stop the platform killing this job!")
-with torch.no_grad():
-    while 1:
-        if a[0] < 1:
-            a += 1
-        else:
-            a -= 1
+a = torch.zeros((1),device=1)
+while(1):
+    pass
