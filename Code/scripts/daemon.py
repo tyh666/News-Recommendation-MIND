8584fbb0d62b762b4de4f9780f09291f926d3c51
namespace-Pt <zpt@ruc.edu.cn>
2021-10-10 15:45:04 +0000

    1

--- Code/scripts/daemon.py
+++ Code/scripts/daemon.py
@@ -8,4 +8,7 @@ logger = logging.getLogger(__file__)
 logger.info("I'm running to stop the platform killing this job!")
 a = torch.zeros((1),device=1)
 while(1):
-    pass
+    if a.item() > 2:
+        a -= 1
+    else:
+        a += 1
