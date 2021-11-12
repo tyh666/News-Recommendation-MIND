2ef43473e34ef3ad8c837a98a8d88e8f118405d2
namespace-Pt <zpt@ruc.edu.cn>
2021-11-12 16:47:19 +0000

    1

--- Code/models/Modules/Synthesizer.py
+++ Code/models/Modules/Synthesizer.py
@@ -11,13 +11,17 @@ class BertSelfAttention(nn.Module):
         self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
         self.all_head_size = self.num_attention_heads * self.attention_head_size
 
-        # self.query = nn.Linear(config.hidden_size, self.all_head_size)
-        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.dense = nn.Sequential(
+            nn.Linear(config.hidden_size, config.signal_length),
+            nn.ReLU(),
+            nn.Linear(config.signal_length, config.signal_length)
+        )
+        self.value_fn = nn.Linear(config.hidden_size, config.hidden_size)
 
         self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
-        self.attention_scores = nn.Parameter(torch.empty(1, config.signal_length, config.signal_length))
 
-        nn.init.xavier_normal_(self.dense.weight)
+        # self.attention_scores = nn.Parameter(torch.empty(1, config.signal_length, config.signal_length))
+        # nn.init.xavier_uniform_(self.attention_scores)
 
     def transpose_for_scores(self, x):
         new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
@@ -34,17 +38,14 @@ class BertSelfAttention(nn.Module):
         past_key_value=None,
         output_attentions=False,
     ):
+        hidden_states = self.value_fn(hidden_states)
         # B, L, L
-        # attention_scores = self.dense(hidden_states)
-        hidden_states = self.dense(hidden_states)
-        
-        attention_mask = attention_mask.squeeze(1)
-        if attention_mask is not None:
-            attention_probs = XSoftmax.apply(self.attention_scores, attention_mask, -1)
+        attention_scores = self.dense(hidden_states)
+        attention_probs = torch.softmax(attention_scores, dim=-1)
 
         # This is actually dropping out entire tokens to attend to, which might
         # seem a bit unusual, but is taken from the original Transformer paper.
-        attention_probs = self.dropout(attention_probs)
+        # attention_probs = self.dropout(attention_probs)
 
         # B, L, D
         context_layer = torch.matmul(attention_probs, hidden_states)
