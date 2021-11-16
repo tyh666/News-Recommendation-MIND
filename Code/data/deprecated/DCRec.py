2f68e1e30b1f6ad5058fbf41a19e028d692c8617
namespace-Pt <zpt@ruc.edu.cn>
2021-11-16 10:08:37 +0800

    1

--- Code/data/deprecated/DCRec.py
+++ Code/data/deprecated/DCRec.py
@@ -1,264 +1,81 @@
-# Two tower baseline with one-tower enhancement
-import math
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from .BaseModel import BaseModel
-from .Encoders.BERT import BERT_Encoder
-from .Rankers.BERT import BERT_Onelayer_Ranker
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
 
-class DCRec(BaseModel):
+from utils.Manager import Manager
+from models.DCRec import DCRec
+
+def main(rank, manager):
+    """ train/dev/test/tune the model (in distributed)
+
+    Args:
+        rank: current process id
+        world_size: total gpus
     """
-    1. encode candidate news with bert
-    2. encode ps terms with the same bert, using [CLS] embedding as user representation
-    3. predict by scaled dot product
-    """
-    def __init__(self, manager, embedding, encoderN, encoderU, reducer):
-        super().__init__(manager)
-        self.hidden_dim = manager.bert_dim
+    manager.setup(rank)
+    loaders = manager.prepare()
 
-        self.embedding = embedding
-        # only these reducers need selection encoding
-        if manager.reducer in manager.get_need_encode_reducers():
-            self.encoderN = encoderN
-            self.encoderU = encoderU
-        self.reducer = reducer
-        self.bert = BERT_Encoder(manager)
-        self.ranker = BERT_Onelayer_Ranker(manager)
+    from models.Embeddings.BERT import BERT_Embedding
+    embedding = BERT_Embedding(manager)
 
-        self.learning2Rank = nn.Sequential(
-            # + 1 because an extra repr matching signal
-            nn.Linear(self.hidden_dim + 1, self.hidden_dim // 2),
-            nn.ReLU(),
-            nn.Linear(self.hidden_dim // 2, 1)
+    if manager.encoderN == 'cnn':
+        from models.Encoders.CNN import CNN_Encoder
+        encoderN = CNN_Encoder(manager)
+    elif manager.encoderN == 'bert':
+        from models.Encoders.BERT import BERT_Encoder
+        encoderN = BERT_Encoder(manager)
+    elif manager.encoderN == 'mha':
+        from models.Encoders.MHA import MHA_Encoder
+        encoderN = MHA_Encoder(manager)
+
+    if manager.encoderU == 'rnn':
+        from models.Encoders.RNN import RNN_User_Encoder
+        encoderU = RNN_User_Encoder(manager)
+    elif manager.encoderU == 'avg':
+        from models.Encoders.Pooling import Average_Pooling
+        encoderU = Average_Pooling(manager)
+    elif manager.encoderU == 'attn':
+        from models.Encoders.Pooling import Attention_Pooling
+        encoderU = Attention_Pooling(manager)
+    elif manager.encoderU == 'mha':
+        from models.Encoders.MHA import MHA_User_Encoder
+        encoderU = MHA_User_Encoder(manager)
+    elif manager.encoderU == 'lstur':
+        from models.Encoders.RNN import LSTUR
+        encoderU = LSTUR(manager)
+
+    if manager.reducer in ['matching', 'bow']:
+        from models.Modules.DRM import Matching_Reducer
+        reducer = Matching_Reducer(manager)
+    elif manager.reducer in ['bm25', "entity", "first"]:
+        from models.Modules.DRM import Identical_Reducer
+        reducer = Identical_Reducer(manager)
+
+    dcrec = DCRec(manager, embedding, encoderN, encoderU, reducer).to(rank)
+
+    if manager.world_size > 1:
+        dcrec = DDP(dcrec, device_ids=[rank], output_device=rank, find_unused_parameters=False)
+
+    if manager.mode == 'dev':
+            manager.evaluate(dcrec, loaders, load=True)
+
+    elif manager.mode == 'train':
+        manager.train(dcrec, loaders)
+
+    elif manager.mode == 'test':
+        manager.test(dcrec, loaders)
+
+    elif manager.mode == 'inspect':
+        manager.inspect(dcrec, loaders)
+
+
+if __name__ == "__main__":
+    manager = Manager()
+    if manager.world_size > 1:
+        mp.spawn(
+            main,
+            args=(manager,),
+            nprocs=manager.world_size,
+            join=True
         )
-        if manager.debias:
-            self.userBias = nn.Parameter(torch.randn(1,self.bert.hidden_dim))
-            nn.init.xavier_normal_(self.userBias)
-
-
-        self.granularity = manager.granularity
-        if self.granularity != 'token':
-            self.register_buffer('cdd_dest', torch.zeros((self.batch_size, self.impr_size, self.signal_length * self.signal_length)), persistent=False)
-            if manager.reducer in ["bm25", "entity", "first"]:
-                self.register_buffer('his_dest', torch.zeros((self.batch_size, self.his_size, (manager.k + 2) * (manager.k + 2))), persistent=False)
-            else:
-                self.register_buffer('his_dest', torch.zeros((self.batch_size, self.his_size, self.signal_length * self.signal_length)), persistent=False)
-
-
-        manager.name = '__'.join(['dcrec', manager.bert, manager.encoderN, manager.encoderU, manager.reducer, manager.granularity, manager.k])
-
-        self.name = manager.name
-
-
-    def clickPredictor(self, cdd_news_repr, user_repr, reduced_vec):
-        """ calculate batch of click probabolity
-
-        Args:
-            cdd_news_repr: news-level representation, [batch_size, cdd_size, hidden_dim]
-            user_repr: user representation, [batch_size, 1, hidden_dim]
-
-        Returns:
-            score of each candidate news, [batch_size, cdd_size]
-        """
-        fused_vec = torch.cat([cdd_news_repr.matmul(user_repr.transpose(-2,-1))/math.sqrt(cdd_news_repr.size(-1)), reduced_vec], dim=-1)
-        score = self.learning2Rank(fused_vec).squeeze(-1)
-        return score
-
-
-    def _forward(self,x):
-        # destroy encoding and embedding outside of the model
-
-        batch_size = x['cdd_encoded_index'].size(0)
-        if self.granularity != 'token':
-            cdd_size = x['cdd_subword_index'].size(1)
-
-            if self.training:
-                cdd_dest = self.cdd_dest[:batch_size, :cdd_size]
-                his_dest = self.his_dest[:batch_size]
-
-            # batch_size always equals 1 when evaluating
-            else:
-                cdd_dest = self.cdd_dest[[0], :cdd_size]
-                his_dest = self.his_dest[[0]]
-
-            cdd_subword_index = x['cdd_subword_index'].to(self.device)
-            his_subword_index = x['his_subword_index'].to(self.device)
-            his_signal_length = his_subword_index.size(-2)
-            cdd_subword_index = cdd_subword_index[:, :, :, 0] * self.signal_length + cdd_subword_index[:, :, :, 1]
-            his_subword_index = his_subword_index[:, :, :, 0] * his_signal_length + his_subword_index[:, :, :, 1]
-
-            if self.training:
-                # * cdd_mask to filter out padded cdd news
-                cdd_subword_prefix = cdd_dest.scatter(dim=-1, index=cdd_subword_index, value=1) * x["cdd_mask"].to(self.device)
-            else:
-                cdd_subword_prefix = cdd_dest.scatter(dim=-1, index=cdd_subword_index, value=1)
-
-            cdd_subword_prefix = cdd_subword_prefix.view(batch_size, cdd_size, self.signal_length, self.signal_length)
-            his_subword_prefix = his_dest.scatter(dim=-1, index=his_subword_index, value=1) * x["his_mask"].to(self.device)
-            his_subword_prefix = his_subword_prefix.view(batch_size, self.his_size, his_signal_length, his_signal_length)
-
-            if self.granularity == 'avg':
-                # average subword embeddings as the word embedding
-                cdd_subword_prefix = F.normalize(cdd_subword_prefix, p=1, dim=-1)
-                his_subword_prefix = F.normalize(his_subword_prefix, p=1, dim=-1)
-
-            cdd_attn_mask = cdd_subword_prefix.matmul(x['cdd_attn_mask'].to(self.device).float().unsqueeze(-1)).squeeze(-1)
-            his_attn_mask = his_subword_prefix.matmul(x["his_attn_mask"].to(self.device).float().unsqueeze(-1)).squeeze(-1)
-            his_refined_mask = None
-            if 'his_refined_mask' in x:
-                his_refined_mask = his_subword_prefix.matmul(x["his_refined_mask"].to(self.device).float().unsqueeze(-1)).squeeze(-1)
-
-        else:
-            cdd_subword_prefix = None
-            his_subword_prefix = None
-            cdd_attn_mask = x['cdd_attn_mask'].to(self.device)
-            his_attn_mask = x["his_attn_mask"].to(self.device)
-            his_refined_mask = None
-            if 'his_refined_mask' in x:
-                his_refined_mask = x["his_refined_mask"].to(self.device)
-
-        cdd_news = x["cdd_encoded_index"].to(self.device)
-        cdd_news_embedding, cdd_news_repr, cdd_attn_mask = self.bert(
-            self.embedding(cdd_news, cdd_subword_prefix), cdd_attn_mask
-        )
-        # cdd_news_repr = self.newsUserProject(cdd_news_repr)
-
-        his_news = x["his_encoded_index"].to(self.device)
-        his_news_embedding = self.embedding(his_news, his_subword_prefix)
-        if hasattr(self, 'encoderN'):
-            his_news_encoded_embedding, his_news_repr = self.encoderN(
-                his_news_embedding, his_attn_mask
-            )
-        else:
-            his_news_encoded_embedding = None
-            his_news_repr = None
-
-        # no need to calculate this if ps_terms are fixed in advance
-        if self.reducer.name == 'matching':
-            user_repr = self.encoderU(his_news_repr, his_mask=x['his_mask'].to(self.device), user_index=x["user_id"].to(self.device))
-        else:
-            user_repr = None
-
-        ps_terms, ps_term_mask, kid = self.reducer(his_news_encoded_embedding, his_news_embedding, user_repr, his_news_repr, his_attn_mask, his_refined_mask)
-
-        ps_terms, user_repr, ps_term_mask = self.bert(ps_terms, ps_term_mask, ps_term_input=True)
-
-        reduced_vec = self.ranker(cdd_news_embedding, ps_terms.squeeze(1), cdd_attn_mask, ps_term_mask)
-
-        if hasattr(self, 'userBias'):
-            user_repr = user_repr + self.userBias
-
-        return self.clickPredictor(cdd_news_repr, user_repr, reduced_vec), kid
-
-
-    def forward(self,x):
-        """
-        Decoupled function, score is unormalized click score
-        """
-        score, kid = self._forward(x)
-
-        if self.training:
-            prob = nn.functional.log_softmax(score, dim=1)
-        else:
-            prob = torch.sigmoid(score)
-
-        return prob, kid
-
-
-    def encode_news(self, x):
-        """
-        encode news of loader_news
-        """
-        # encode news with MIND_news
-        if self.granularity != 'token':
-            batch_size = x['cdd_subword_index'].size(0)
-            cdd_dest = self.cdd_dest[:batch_size]
-            cdd_subword_index = x['cdd_subword_index'].to(self.device)
-            cdd_subword_index = cdd_subword_index[:, :, 0] * self.signal_length + cdd_subword_index[:, :, 1]
-
-            cdd_subword_prefix = cdd_dest.scatter(dim=-1, index=cdd_subword_index, value=1)
-            cdd_subword_prefix = cdd_subword_prefix.view(batch_size, self.signal_length, self.signal_length)
-
-            if self.granularity == 'avg':
-                # average subword embeddings as the word embedding
-                cdd_subword_prefix = F.normalize(cdd_subword_prefix, p=1, dim=-1)
-            cdd_attn_mask = cdd_subword_prefix.matmul(x['cdd_attn_mask'].to(self.device).float().unsqueeze(-1)).squeeze(-1)
-
-        else:
-            cdd_subword_prefix = None
-            cdd_attn_mask = x['cdd_attn_mask'].to(self.device)
-
-        cdd_news = x["cdd_encoded_index"].to(self.device)
-        _, cdd_news_repr = self.bert(
-            self.embedding(cdd_news, cdd_subword_prefix), cdd_attn_mask
-        )
-        # cdd_news_repr = self.newsUserProject(cdd_news_repr.squeeze(1))
-
-        return cdd_news_repr.squeeze(1)
-
-
-    def predict_fast(self, x):
-        """
-        1. encode user
-        2. look up candidate representation in the embedding matrix
-        3. compute click probability
-        """
-        if self.granularity != 'token':
-            batch_size = x['his_encoded_index'].size(0)
-            his_dest = self.his_dest[:batch_size]
-
-            his_subword_index = x['his_subword_index'].to(self.device)
-            his_signal_length = his_subword_index.size(-2)
-            his_subword_index = his_subword_index[:, :, :, 0] * his_signal_length + his_subword_index[:, :, :, 1]
-
-            his_subword_prefix = his_dest.scatter(dim=-1, index=his_subword_index, value=1) * x["his_mask"].to(self.device)
-            his_subword_prefix = his_subword_prefix.view(batch_size, self.his_size, his_signal_length, his_signal_length)
-
-            if self.granularity == 'avg':
-                # average subword embeddings as the word embedding
-                his_subword_prefix = F.normalize(his_subword_prefix, p=1, dim=-1)
-
-            his_attn_mask = his_subword_prefix.matmul(x["his_attn_mask"].to(self.device).float().unsqueeze(-1)).squeeze(-1)
-            his_refined_mask = None
-            if 'his_refined_mask' in x:
-                his_refined_mask = his_subword_prefix.matmul(x["his_refined_mask"].to(self.device).float().unsqueeze(-1)).squeeze(-1)
-
-        else:
-            his_subword_prefix = None
-            his_attn_mask = x["his_attn_mask"].to(self.device)
-            his_refined_mask = None
-            if 'his_refined_mask' in x:
-                his_refined_mask = x["his_refined_mask"].to(self.device)
-
-        his_news = x["his_encoded_index"].to(self.device)
-        his_news_embedding = self.embedding(his_news, his_subword_prefix)
-        if hasattr(self, 'encoderN'):
-            his_news_encoded_embedding, his_news_repr = self.encoderN(
-                his_news_embedding, his_attn_mask
-            )
-        else:
-            his_news_encoded_embedding = None
-            his_news_repr = None
-        # no need to calculate this if ps_terms are fixed in advance
-        if self.reducer.name == 'matching':
-            user_repr = self.encoderU(his_news_repr, his_mask=x['his_mask'].to(self.device), user_index=x['user_id'].to(self.device))
-        else:
-            user_repr = None
-
-        ps_terms, ps_term_mask, _ = self.reducer(his_news_encoded_embedding, his_news_embedding, user_repr, his_news_repr, his_attn_mask, his_refined_mask)
-
-        _, user_repr = self.bert(ps_terms, ps_term_mask, ps_term_input=True)
-
-        if self.aggregator is not None:
-            user_repr = self.aggregator(user_repr)
-        # user_repr = self.newsUserProject(user_cls)
-        if hasattr(self, 'userBias'):
-            user_repr = user_repr + self.userBias
-
-        # [bs, cs, hd]
-        news_repr = self.news_reprs(x['cdd_id'].to(self.device))
-
-        scores = news_repr.matmul(user_repr.transpose(-1, -2)).squeeze(-1)/math.sqrt(news_repr.size(-1))
-        logits = torch.sigmoid(scores)
-        return logits
+    else:
+        main(manager.device, manager)
--- Code/data/deprecated/ESM.py
+++ Code/data/deprecated/ESM.py
@@ -1,134 +1,87 @@
-# Personalized terms
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from .BaseModel import BaseModel
+import torch.multiprocessing as mp
+from torch.nn.modules.rnn import RNN
+from torch.nn.parallel import DistributedDataParallel as DDP
 
-class ESM(BaseModel):
-    def __init__(self, manager, embedding, encoderN, encoderU, reducer, ranker):
-        super().__init__(manager)
+from utils.Manager import Manager
+from models.ESM import ESM
 
-        self.embedding = embedding
-        self.encoderN = encoderN
-        self.encoderU = encoderU
-        self.reducer = reducer
-        self.ranker = ranker
+def main(rank, manager):
+    """ train/dev/test/tune the model (in distributed)
 
-        self.final_dim = ranker.hidden_dim
+    Args:
+        rank: current process id
+        world_size: total gpus
+    """
+    manager.setup(rank)
+    loaders = manager.prepare()
 
-        self.learningToRank = nn.Sequential(
-            nn.Linear(self.final_dim, int(self.final_dim/2)),
-            nn.Tanh(),
-            nn.Linear(int(self.final_dim/2),1)
+    from models.Embeddings.BERT import BERT_Embedding
+    embedding = BERT_Embedding(manager)
+    if manager.encoderN == 'cnn':
+        from models.Encoders.CNN import CNN_Encoder
+        encoderN = CNN_Encoder(manager)
+    elif manager.encoderN == 'rnn':
+        from models.Encoders.RNN import RNN_Encoder
+        encoderN = RNN_Encoder(manager)
+    elif manager.encoderN == 'mha':
+        from models.Encoders.MHA import MHA_Encoder
+        encoderN = MHA_Encoder(manager)
+
+    if manager.encoderU == 'avg':
+        from models.Encoders.Pooling import Average_Pooling
+        encoderU = Average_Pooling(manager)
+    elif manager.encoderU == 'cnn':
+        from models.Encoders.CNN import CNN_User_Encoder
+        encoderU = CNN_User_Encoder(manager)
+    elif manager.encoderU == 'rnn':
+        from models.Encoders.RNN import RNN_User_Encoder
+        encoderU = RNN_User_Encoder(manager)
+    elif manager.encoderU == 'mha':
+        from models.Encoders.MHA import MHA_User_Encoder
+        encoderU = MHA_User_Encoder(manager)
+
+    if manager.reducer in ['matching', 'bow']:
+        from models.Modules.DRM import Matching_Reducer
+        reducer = Matching_Reducer(manager)
+    elif manager.reducer in ['bm25', "entity", "first"]:
+        from models.Modules.DRM import Identical_Reducer
+        reducer = Identical_Reducer(manager)
+
+    if manager.ranker == 'onepass':
+        from models.Rankers.BERT import BERT_Onepass_Ranker
+        ranker = BERT_Onepass_Ranker(manager)
+    elif manager.ranker == 'original':
+        from models.Rankers.BERT import BERT_Original_Ranker
+        ranker = BERT_Original_Ranker(manager)
+    elif manager.ranker == 'cnn':
+        from models.Rankers.CNN import CNN_Ranker
+        ranker = CNN_Ranker(manager)
+
+    esm = ESM(manager, embedding, encoderN, encoderU, reducer, ranker).to(rank)
+
+    if manager.world_size > 1:
+        esm = DDP(esm, device_ids=[rank], output_device=rank, find_unused_parameters=False)
+
+    if manager.mode == 'train':
+        manager.train(esm, loaders)
+
+    elif manager.mode == 'dev':
+        manager.evaluate(esm, loaders, load=True)
+
+    elif manager.mode == 'test':
+        manager.test(esm, loaders)
+
+    elif manager.mode == 'inspect':
+        manager.inspect(esm, loaders)
+
+
+if __name__ == "__main__":
+    manager = Manager()
+    if manager.world_size > 1:
+        mp.spawn(
+            main,
+            args=(manager,),
+            nprocs=manager.world_size
         )
-        nn.init.xavier_normal_(self.learningToRank[0].weight)
-        nn.init.xavier_normal_(self.learningToRank[2].weight)
-
-        self.granularity = manager.granularity
-        if self.granularity != 'token':
-            self.register_buffer('cdd_dest', torch.zeros((self.batch_size, manager.impr_size, manager.signal_length * manager.signal_length)), persistent=False)
-            if manager.reducer in ["bm25", "entity", "first"]:
-                self.register_buffer('his_dest', torch.zeros((self.batch_size, self.his_size, (manager.k + 1) * (manager.k + 1))), persistent=False)
-            else:
-                self.register_buffer('his_dest', torch.zeros((self.batch_size, self.his_size, manager.signal_length * manager.signal_length)), persistent=False)
-
-        manager.name = '__'.join(['esm', manager.embedding, manager.encoderN, manager.encoderU, manager.reducer, manager.ranker, manager.granularity, "full" if manager.full_attn else "partial", str(manager.k)])
-
-
-    def clickPredictor(self, reduced_tensor):
-        """ calculate batch of click probabolity
-
-        Args:
-            reduced_tensor: [batch_size, cdd_size, final_dim]
-            cdd_news_repr: news-level representation, [batch_size, cdd_size, hidden_dim]
-            user_repr: user representation, [batch_size, 1, hidden_dim]
-
-        Returns:
-            score of each candidate news, [batch_size, cdd_size]
-        """
-
-        # print(user_repr.mean(), cdd_news_repr.mean(), user_repr.max(), cdd_news_repr.max(), user_repr.sum(), cdd_news_repr.sum())
-        # score_coarse = cdd_news_repr.matmul(user_repr.transpose(-2,-1))
-        # score = torch.cat([reduced_tensor, score_coarse], dim=-1)
-        score = self.learningToRank(reduced_tensor).squeeze(dim=-1)
-
-        return score
-
-    def _forward(self,x):
-        if self.granularity != 'token':
-            batch_size = x['cdd_subword_index'].size(0)
-            cdd_size = x['cdd_subword_index'].size(1)
-
-            if self.training:
-                cdd_dest = self.cdd_dest[:batch_size, :cdd_size]
-                his_dest = self.his_dest[:batch_size]
-
-            # batch_size always equals 1 when evaluating
-            else:
-                cdd_dest = self.cdd_dest[[0], :cdd_size]
-                his_dest = self.his_dest[[0]]
-
-            cdd_subword_index = x['cdd_subword_index'].to(self.device)
-            his_subword_index = x['his_subword_index'].to(self.device)
-            his_signal_length = his_subword_index.size(-2)
-            cdd_subword_index = cdd_subword_index[:, :, :, 0] * self.signal_length + cdd_subword_index[:, :, :, 1]
-            his_subword_index = his_subword_index[:, :, :, 0] * his_signal_length + his_subword_index[:, :, :, 1]
-
-            if self.training:
-                # * cdd_mask to filter out padded cdd news
-                cdd_subword_prefix = cdd_dest.scatter(dim=-1, index=cdd_subword_index, value=1) * x["cdd_mask"].to(self.device)
-            else:
-                cdd_subword_prefix = cdd_dest.scatter(dim=-1, index=cdd_subword_index, value=1)
-            cdd_subword_prefix = cdd_subword_prefix.view(batch_size, cdd_size, self.signal_length, self.signal_length)
-
-            his_subword_prefix = his_dest.scatter(dim=-1, index=his_subword_index, value=1) * x["his_mask"].to(self.device)
-            his_subword_prefix = his_subword_prefix.view(batch_size, self.his_size, his_signal_length, his_signal_length)
-
-            if self.granularity == 'avg':
-                # average subword embeddings as the word embedding
-                cdd_subword_prefix = F.normalize(cdd_subword_prefix, p=1, dim=-1)
-                his_subword_prefix = F.normalize(his_subword_prefix, p=1, dim=-1)
-
-            cdd_attn_mask = cdd_subword_prefix.matmul(x['cdd_attn_mask'].to(self.device).float().unsqueeze(-1)).squeeze(-1)
-            his_attn_mask = his_subword_prefix.matmul(x["his_attn_mask"].to(self.device).float().unsqueeze(-1)).squeeze(-1)
-            his_refined_mask = None
-            if 'his_refined_mask' in x:
-                his_refined_mask = his_subword_prefix.matmul(x["his_refined_mask"].to(self.device).float().unsqueeze(-1)).squeeze(-1)
-
-        else:
-            cdd_subword_prefix = None
-            his_subword_prefix = None
-            cdd_attn_mask = x['cdd_attn_mask'].to(self.device)
-            his_attn_mask = x["his_attn_mask"].to(self.device)
-            his_refined_mask = None
-            if 'his_refined_mask' in x:
-                his_refined_mask = x["his_refined_mask"].to(self.device)
-
-
-        cdd_news = x["cdd_encoded_index"].long().to(self.device)
-        cdd_news_embedding = self.embedding(cdd_news, cdd_subword_prefix)
-
-        his_news = x["his_encoded_index"].long().to(self.device)
-        his_news_embedding = self.embedding(his_news, his_subword_prefix)
-        his_news_encoded_embedding, his_news_repr = self.encoderN(his_news_embedding, his_attn_mask)
-
-        user_repr = self.encoderU(his_news_repr)
-
-        ps_terms, ps_term_mask, kid = self.reducer(his_news_encoded_embedding, his_news_embedding, user_repr, his_news_repr, his_attn_mask, his_refined_mask)
-
-        reduced_tensor = self.ranker(cdd_news_embedding, ps_terms, cdd_attn_mask, ps_term_mask)
-
-        return self.clickPredictor(reduced_tensor), kid
-
-    def forward(self,x):
-        """
-        Decoupled function, score is unormalized click score
-        """
-        score, kid = self._forward(x)
-
-        if self.training:
-            logit = nn.functional.log_softmax(score, dim=1)
-        else:
-            logit = torch.sigmoid(score)
-
-        return logit, kid
+    else:
+        main(manager.device, manager)
